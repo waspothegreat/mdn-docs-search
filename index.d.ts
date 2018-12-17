bf6a636e7a9f3f074620a38e352c13ecabbc63e9
Nicolas <nicolas.husereau@theojcs.ca>
2018-12-17 12:29:54 -0500

    update typings

--- index.d.ts
+++ index.d.ts
@@ -3,20 +3,20 @@ declare class MDNDocs {
     public static load(result: string): Promise<MDNDocResult>;
 }
 
-class MDNDocResult {
-    public constructor(public text: string);
+declare class MDNDocResult {
+    public constructor(text: string);
     public readonly $: Promise<object>;
-    public get name(): Promise<string>;
-    public get description(): Promise<string>;
-    public get url(): Promise<string>;
-    public get examples(): Promise<string | null>;
-    public get polyfill(): Promise<string | null>;
-    public get syntax(): Promise<string | null>;
-    public get params(): Promise<Array<Array<string>> | null>;
-    public get pageDescription(): Promise<string | null>;
-    public get seeAlso(): Promise<string | null>;
-    public get methods(): Promise<Array<Array<string>>| null>;
-    public get returnValue(): Promise<string | null>;
+    public name: Promise<string>;
+    public description: Promise<string>;
+    public url: Promise<string>;
+    public examples: Promise<string | null>;
+    public polyfill: Promise<string | null>;
+    public syntax: Promise<string | null>;
+    public params: Promise<string[][] | null>;
+    public pageDescription: Promise<string | null>;
+    public seeAlso: Promise<string | null>;
+    public methods: Promise<string[][] | null>;
+    public returnValue: Promise<string | null>;
 }
 
 export = MDNDocs;
