0ea922beaef7701f7a105cf8ed8a8121ded4dccb
Nicolas <nicolas.husereau@theojcs.ca>
2018-12-23 21:06:53 -0500

    re-add typings and final version

---
+++ index.d.ts
@@ -0,0 +1,22 @@
+declare class MDNDocs {
+    public static search(query?: any): Promise<string>;
+    public static load(result: any): Promise<MDNDocResult>;
+}
+
+declare class MDNDocResult {
+    public constructor(text: any);
+    public readonly $: Promise<object>;
+    public name: Promise<string>;
+    public description: Promise<string>;
+    public url: Promise<string>;
+    public examples: Promise<string>;
+    public polyfill: Promise<string>;
+    public syntax: Promise<string>;
+    public params: Promise<Array<Array<string>>>;
+    public pageDescription: Promise<string>;
+    public seeAlso: Promise<string>;
+    public methods: Promise<Array<Array<string>>>;
+    public returnValue: Promise<string>;
+}
+
+export = MDNDocs;
